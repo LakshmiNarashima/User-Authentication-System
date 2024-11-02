0c0e767b719bf4e5e466fecf60e9e8f260e1df62
Lakshmi Narashima R <lakshminarashimar49@gmail.com>
2024-11-02 21:23:52 +0530

    models.py
    Database Model for User

---
+++ models.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
